2a0a4d5ad40aca16cea49814c9341d4644f50daa
wellinton-Silveira <wellintonhf123@gmail.com>
2024-09-17 17:27:14 -0300

    Toolbar foi inclusa

--- src/main/java/Controller/CotacoesControllerBean.java
+++ src/main/java/Controller/CotacoesControllerBean.java
@@ -12,11 +12,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.primefaces.model.chart.Axis;
-import org.primefaces.model.chart.AxisType;
-import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.ChartSeries;
-
 import DTO.FiltroDTO;
 import DTO.IndicadorDTO;
 import entity.Cotacoes;
@@ -53,10 +48,6 @@ public class CotacoesControllerBean implements Serializable {
     private String startDate;
     private String finalDate;
     
-    private BarChartModel barChartModel;
-    
-    private String iconSearch = "pi pi-search";
-    private String iconSave = "pi pi-save";
     
     private List<Cotacoes> listaCotacoes;
     private List<Indicadores> listaIndicadores;
@@ -227,7 +218,6 @@ public class CotacoesControllerBean implements Serializable {
                 FacesContext.getCurrentInstance().addMessage(null,
                         new FacesMessage(FacesMessage.SEVERITY_WARN, "Aviso", "Selecione um filtro válido."));
         }
-        createBarModel();
     }
     
     
@@ -264,48 +254,7 @@ public class CotacoesControllerBean implements Serializable {
     	return dataBase.minusDays(dias);
     }
     
-    public BarChartModel getBarChartModel() {
-    	if (barChartModel == null) {
-    		createBarModel();
-    	}
-    	return barChartModel;
-    }
     
-    private void createBarModel() {
-        if (cotacoesFiltradas == null) {
-            cotacoesFiltradas = cotacoesService.buscarPorPeriodoEIndicador(dataInicial, dataFinal, indicadorId);
-        }
-        
-        barChartModel = new BarChartModel();
-        
-        ChartSeries cotacoesSeries = new ChartSeries();
-        cotacoesSeries.setLabel("Cotações");
-
-        for (FiltroDTO cotacao : cotacoesFiltradas) {
-        	 LocalDateTime dateTime = cotacao.getDataHora();
-             
-             String formattedDate = dateTime.format(FORMATTER);
-             Double value = cotacao.getValor();
-            cotacoesSeries.set(formattedDate, value);
-        }
-
-        barChartModel.addSeries(cotacoesSeries);
-        
-        barChartModel.setTitle("Gráfico de Cotações");
-        barChartModel.setLegendPosition("ne");
-        barChartModel.setAnimate(true);
-        barChartModel.setShowDatatip(true);
-        barChartModel.setExtender("customizeDatatip");
-
-        Axis xAxis = barChartModel.getAxis(AxisType.X);
-        xAxis.setLabel("Data e Valor");
-        xAxis.setTickAngle(-45);
-        xAxis.setTickInterval("1");
-
-        Axis yAxis = barChartModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Valor");
-        yAxis.setMin(0);
-    }
 
 	public Cotacoes getSelectedCotacao() {
         return selectedCotacao;
@@ -398,39 +347,6 @@ public class CotacoesControllerBean implements Serializable {
 
 	public void setFinalDate(String finalDate) {
 		this.finalDate = finalDate;
-	}
-
-	
-	public void changeIconLoad() {
-        this.iconSearch = "pi pi-spin pi-spinner";
-    }
-	
-	public void changeIconLoadSave() {
-        this.iconSave = "pi pi-spin pi-spinner";
-    }
-	
-	public void initIconSearch() {
-		this.iconSearch = "pi pi-search";
-	}
-	
-	public void initIconSave() {
-		this.iconSave = "pi pi-save";
-	}
-
-	public String getIconSearch() {
-		return iconSearch;
-	}
-
-	public void setIconSearch(String iconSearch) {
-		this.iconSearch = iconSearch;
-	}
-
-	public String getIconSave() {
-		return iconSave;
-	}
-
-	public void setIconSave(String iconSave) {
-		this.iconSave = iconSave;
 	}
 
 }
